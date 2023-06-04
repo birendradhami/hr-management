30d275a77349061e31ba8fb28ad361c03a7e8c80
Birendra Dhami <sbirendradhami@gmail.com>
2023-06-04 18:55:58 +0545

    N-tier Architecture

---
+++ HRM.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using HRM.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRM.DataAccess.Data;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+                   
+
+        } public DbSet<Employee> Employees { get; set; }
+          public DbSet<Designation>Designations { get; set; }
+          public DbSet<Department> Departments { get; set; }
+    }
+
---
+++ HRM.Models/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace HRM.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Full Name")]
+        public string? Name { get; set; }
+        [Required]
+        public string? Address { get; set; }
+        [Required]
+        public string? Phone { get; set; }
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        public Gender Gender { get; set; }
+
+        [Required]
+        [DisplayName("Join Date")]
+        [DataType(DataType.Date)]
+        public DateTime JoinDate { get; set; }
+
+        public Designation? Designation { get; set; }
+
+        [DisplayName("Designation")]
+        public int DesignationId { get; set; }
+
+        public Department? Department { get; set; }
+
+        [DisplayName("Department")]
+        public int DepartmentId { get; set; }
+
+    }
+}
